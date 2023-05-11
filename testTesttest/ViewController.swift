ddad5fd722585b970556994e7fa3bf544ad61831
Fahima <faisfahima@gmail.com>
2023-05-11 06:58:08 -0700

    added function

--- testTesttest/ViewController.swift
+++ testTesttest/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Hello")
     }
-
+    func what(){
+        print("somethingelse")
+    }
 
 }
 
